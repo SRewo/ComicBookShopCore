3ba8b518edcafdd8ffd4007a290ef8f191f1c45b
SRewo <maoam97@gmail.com>
2019-04-04 17:23:35 +0200

    Add asp.net mvc project files and few changes

---
+++ ComicBookShopCore.Web/Models/LayoutViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ComicBookShopCore.Data;
+using ComicBookShopCore.Data.Repositories;
+
+namespace ComicBookShopCore.Web.Models
+{
+    public class LayoutViewModel
+    {
+        private SqlRepository<Publisher> _publisheRepository;
+
+
+        public List<Publisher> Publishers { get; set; }
+
+        public LayoutViewModel()
+        {
+            using (var context = new ShopDbEntities())
+            {
+                _publisheRepository = new SqlRepository<Publisher>(context);
+                Publishers = _publisheRepository.GetAll().ToList();
+            }
+        }
+    }
+}
