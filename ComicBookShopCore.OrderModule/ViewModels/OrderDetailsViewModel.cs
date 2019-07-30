e9353a6b6d5c19643639a15f356d9f85b4309cea
SRewo <maoam97@gmail.com>
2019-07-30 15:05:33 +0200

    Add order details view

--- ComicBookShopCore.OrderModule/ViewModels/OrderDetailsViewModel.cs
+++ ComicBookShopCore.OrderModule/ViewModels/OrderDetailsViewModel.cs
@@ -1,10 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Threading.Tasks;
+using ComicBookShopCore.Data;
+using Prism.Commands;
+using Prism.Regions;
 
 namespace ComicBookShopCore.OrderModule.ViewModels
 {
-    class OrderDetailsViewModel
+    public class OrderDetailsViewModel : ValidableBase, INavigationAware
     {
+        private readonly IRegionManager _manager;
+
+        private Order _order;
+
+        public OrderDetailsViewModel(IRegionManager manager)
+        {
+            _manager = manager;
+            GoBackCommand = new DelegateCommand(() => GoBackAsync());
+        }
+
+        public DelegateCommand GoBackCommand { get; set; }
+
+        public Order Order
+        {
+            get => _order;
+            set => SetProperty(ref _order, value);
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            Order ??= (Order) navigationContext.Parameters["Order"];
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+        }
+
+        public Task GoBackAsync()
+        {
+            _manager.RequestNavigate("content", "OrderList");
+
+            return Task.CompletedTask;
+        }
     }
 }
