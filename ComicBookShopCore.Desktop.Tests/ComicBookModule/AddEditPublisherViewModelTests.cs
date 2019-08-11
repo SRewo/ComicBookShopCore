fa25959fe2151c2bde601d25853e2ae7c7f31a2f
SRewo <maoam97@gmail.com>
2019-08-11 11:10:19 +0200

    Add publisher view model and tests update

--- ComicBookShopCore.Desktop.Tests/ComicBookModule/AddEditPublisherViewModelTests.cs
+++ ComicBookShopCore.Desktop.Tests/ComicBookModule/AddEditPublisherViewModelTests.cs
@@ -6,6 +6,7 @@ using Moq;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -13,185 +14,160 @@ namespace ComicBookShopCore.Desktop.Tests.ComicBookModule
 {
     public class AddEditPublisherViewModelTests
     {
-        //[Fact]
-        //public void OnNavigatedTo_WithoutParameters_ValidCall()
-        //{
-        //    var model = new AddEditPublisherViewModel(null, null);
-        //    model.OnNavigatedTo(new NavigationContext(null, null));
+        [Fact]
+        public void OnNavigatedTo_ValidCall()
+        {
+            var mock = new Mock<AddEditPublisherViewModel>(null,null);
+            var model = mock.Object;
+            model.OnNavigatedTo(new NavigationContext(null, null));
 
-        //    Assert.NotNull(model.Publisher);
-        //}
+            mock.Verify(x => x.CheckPassedPublisherAsync(null), Times.Once);
+            mock.Verify(x => x.ResetModelAsync(), Times.Once);
+            mock.Verify(x => x.SetErrorsChangedEventAsync(), Times.Once);
+        }
 
-        //[Fact]
-        //public void OnNavigatedTo_WithParameters_ValidCall()
-        //{
-        //    using var mock = AutoMock.GetLoose();
-        //    var uri = new Uri("test?name=value", UriKind.Relative);
-        //    var publisher = new Publisher(1)
-        //    {
-        //        Name = "Dc Comics",
-        //    };
-        //    var parameters = new NavigationParameters()
-        //    {
-        //        {"publisher", publisher}
-        //    };
-        //    var model = new AddEditPublisherViewModel(null, null);
+        [Fact]
+        public void CheckPassedPublisher_WithoutPublisher_ValidCall()
+        {
+            var model = new AddEditPublisherViewModel(null, null);
+            model.CheckPassedPublisherAsync(null);
 
+            Assert.Null(model.Publisher);
+            Assert.Null(model.InputModel.Name);
+            Assert.Null(model.InputModel.Description);
+            Assert.NotEqual(DateTime.MinValue, model.InputModel.CreationDateTime);
+        }
 
-        //    IRegion region = new Region();
-        //    mock.Mock<IRegionNavigationService>().Setup(x => x.Region).Returns(region);
-        //    var navigationService = mock.Create<IRegionNavigationService>();
+        [Fact]
+        public void NameErrorsMessage_DisplaysProperMessage()
+        {
+            var model = new AddEditPublisherViewModel(null, null);
+            model.CheckPassedPublisherAsync(null);
+            model.SetErrorsChangedEventAsync();
 
+            model.InputModel.Name = "";
 
-        //    var navigationContext = new NavigationContext(navigationService, uri, parameters);
-        //    model.OnNavigatedTo(navigationContext);
+            var expectedMessage = "Publisher name cannot be empty.";
+            var actualMessage = model.NameErrorMessage;
 
-        //    Assert.NotNull(model.Publisher);
-        //    Assert.Equal(publisher.Name, model.Publisher.Name);
-        //    Assert.Equal(publisher.Id, model.Publisher.Id);
-        //}
+            Assert.True(model.InputModel.HasErrors);
+            Assert.False(model.CanSave);
+            Assert.Equal(expectedMessage, actualMessage);
+        }
 
-        //[Fact]
-        //public void NameErrorsMessage_DisplaysProperMessage()
-        //{
-        //    var model = new AddEditPublisherViewModel(null, null);
-        //    var publisher = new Publisher(1)
-        //    {
-        //        Name = "Dc Comics",
-        //    };
-        //    model.Publisher = publisher;
-        //    model.Publisher.Name = "";
-        //    model.Publisher_ErrorsChanged(null, null);
+        [Fact]
+        public void DateErrorMessage_DisplaysProperMessage()
+        {
 
-        //    var expectedMessage = publisher.GetFirstError();
-        //    var actualMessage = model.NameErrorMessage;
+            var model = new AddEditPublisherViewModel(null, null);
 
-        //    Assert.True(model.Publisher.HasErrors);
-        //    Assert.Equal(expectedMessage, actualMessage);
-        //}
+            model.CheckPassedPublisherAsync(null);
+            model.SetErrorsChangedEventAsync();
+            model.InputModel.CreationDateTime = DateTime.MaxValue;
 
-        //[Fact]
-        //public void DateErrorMessage_DisplaysProperMessage()
-        //{
-        //    var model = new AddEditPublisherViewModel(null, null);
-        //    var publisher = new Publisher(1)
-        //    {
-        //        Name = "Dc Comics",
-        //    };
-        //    model.Publisher = publisher;
-        //    model.Publisher.ErrorsChanged += model.Publisher_ErrorsChanged;
-        //    model.Publisher.CreationDateTime = DateTime.MaxValue;
+            var expectedMessage = "You have to choose a date between 01.01.1900 and today";
+            var actualMessage = model.DateErrorMessage;
 
-        //    var expectedMessage = "You have to choose a date between 01.01.1900 and today";
-        //    var actualMessage = model.DateErrorMessage;
+            Assert.True(model.InputModel.HasErrors);
+            Assert.Equal(expectedMessage, actualMessage);
+        }
 
-        //    Assert.True(model.Publisher.HasErrors);
-        //    Assert.Equal(expectedMessage, actualMessage);
-        //}
+        [Fact]
+        public void GoBack_ValidCall()
+        {
+            using var mock = AutoMock.GetLoose();
+            var model = mock.Create<AddEditPublisherViewModel>();
+            model.GoBackCommand.Execute();
 
-        //[Fact]
-        //public void GoBack_ValidCall()
-        //{
-        //    using var mock = AutoMock.GetLoose();
-        //    var model = mock.Create<AddEditPublisherViewModel>();
-        //    model.GoBackCommand.Execute();
+            mock.Mock<IRegionManager>().Verify(x => x.RequestNavigate("content", "PublisherList"), Times.Once);
+        }
 
-        //    mock.Mock<IRegionManager>().Verify(x => x.RequestNavigate("content", "PublisherList"), Times.Once);
-        //}
+        [Fact]
+        public void SavePublisherCommand_Adding_ValidExecute()
+        {
 
-        //[Fact]
-        //public void SavePublisherCommand_Adding_ValidExecute()
-        //{
-        //    using var mock = AutoMock.GetLoose();
-        //    var publisher = new Publisher()
-        //    {
-        //        Name = "Dc Comics",
-        //        CreationDateTime = new DateTime(2010, 01, 01)
-        //    };
-        //    var model = mock.Create<AddEditPublisherViewModel>();
-        //    model.Publisher = publisher;
-        //    model.SavePublisherCommand.Execute();
+            using var mock = AutoMock.GetLoose();
+            var publisher = TestData.GetPublishersSample().First();
+            var model = mock.Create<AddEditPublisherViewModel>();
+            model.SetErrorsChangedEventAsync();
+            model.CheckPassedPublisherAsync(null);
 
-        //    mock.Mock<IRepository<Publisher>>().Verify(x => x.Add(publisher), Times.Once);
-        //}
+            model.InputModel.Name = publisher.Name;
+            model.InputModel.Description = publisher.Description;
+            model.InputModel.CreationDateTime = publisher.CreationDateTime;
 
-        //[Fact]
-        //public void SavePublisherComand_Editing_ValidExecute()
-        //{
-        //    using var mock = AutoMock.GetLoose();
-        //    var publisher = new Publisher(1)
-        //    {
-        //        Name = "Dc Comics",
-        //        CreationDateTime = new DateTime(2010, 01, 01)
-        //    };
-        //    var model = mock.Create<AddEditPublisherViewModel>();
-        //    model.Publisher = publisher;
-        //    model.SavePublisherCommand.Execute();
+            Assert.False(model.IsEditing);
+            Assert.True(model.CanSave);
+            model.SavePublisherCommand.Execute();
 
-        //    mock.Mock<IRepository<Publisher>>().Verify(x => x.Update(publisher), Times.Once);
-        //}
+            mock.Mock<IRepository<Publisher>>().Verify(x => x.Add(model.Publisher), Times.Once);
+        }
 
-        //[Fact]
-        //public void CanExecuteChanged_HasErrors_CanSaveIsFalse()
-        //{
-        //    var model = new AddEditPublisherViewModel(null, null);
-        //    var publisher = new Publisher(1)
-        //    {
-        //        Name = "Dc Comics",
-        //        CreationDateTime = new DateTime(2010, 01, 01)
-        //    };
-        //    model.Publisher = publisher;
-        //    model.Publisher.Name = "t";
-        //    model.CanExecuteChanged(null, null);
+        [Fact]
+        public void SavePublisherCommand_Editing_ValidExecute()
+        {
 
-        //    Assert.False(model.CanSave);
-        //}
+            var publisher = TestData.GetPublishersSample().First();
+            using var mock = AutoMock.GetLoose();
+            var model = mock.Create<AddEditPublisherViewModel>();
+            model.CheckPassedPublisherAsync(publisher);
+            model.SetErrorsChangedEventAsync();
 
-        //[Fact]
-        //public void CanExecuteChanged_WithoutErrors_CanSaveIsTrue()
-        //{
-        //    var model = new AddEditPublisherViewModel(null, null);
-        //    var publisher = new Publisher(1)
-        //    {
-        //        Name = "Dc Comics",
-        //    };
-        //    model.Publisher = publisher;
-        //    model.Publisher.CreationDateTime = new DateTime(2010, 01, 01);
-        //    model.CanExecuteChanged(null, null);
+            model.InputModel.Name = "Test name";
+            model.SavePublisherCommand.Execute();
 
-        //    Assert.True(model.CanSave);
-        //}
+            mock.Mock<IRepository<Publisher>>().Verify(x => x.Update(model.Publisher), Times.Once);
+        }
 
-        //[Fact]
-        //public void CanExecuteChanged_NameIsEmpty_CanSaveIsFalse()
-        //{
-        //    var model = new AddEditPublisherViewModel(null, null);
-        //    var publisher = new Publisher(1)
-        //    {
-        //        Name = "Dc Comics",
-        //        CreationDateTime = new DateTime(2010, 01, 01)
-        //    };
-        //    model.Publisher = publisher;
-        //    model.Publisher.Name = "";
-        //    model.CanExecuteChanged(null, null);
+        [Fact]
+        public void CanExecuteChanged_HasErrors_CanSaveIsFalse()
+        {
+            var model = new AddEditPublisherViewModel(null, null);
+            var publisher = TestData.GetPublishersSample().First();
+            model.SetErrorsChangedEventAsync();
+            model.InputModel.Name = "t";
 
-        //    Assert.False(model.CanSave);
-        //}
+            Assert.False(model.CanSave);
+        }
 
-        //[Fact]
-        //public void CanExecuteChanged_CreationDateTimeIsMin_CanSaveIsFalse()
-        //{
-        //    var model = new AddEditPublisherViewModel(null, null);
-        //    var publisher = new Publisher(1)
-        //    {
-        //        Name = "Dc Comics",
-        //        CreationDateTime = new DateTime(2010, 01, 01)
-        //    };
-        //    model.Publisher = publisher;
-        //    model.Publisher.CreationDateTime = DateTime.MinValue;
-        //    model.CanExecuteChanged(null, null);
+        [Fact]
+        public void CanExecuteChanged_WithoutErrors_CanSaveIsTrue()
+        {
+            var model = new AddEditPublisherViewModel(null, null);
+            var publisher = TestData.GetPublishersSample().First();
+            model.CheckPassedPublisherAsync(publisher);
+            model.SetErrorsChangedEventAsync();
 
-        //    Assert.False(model.CanSave);
-        //}
+            model.InputModel.CreationDateTime = new DateTime(2010, 01, 01);
+
+            Assert.True(model.CanSave);
+        }
+
+        [Fact]
+        public void CanExecuteChanged_NameIsEmpty_CanSaveIsFalse()
+        {
+            var model = new AddEditPublisherViewModel(null, null);
+            var publisher = TestData.GetPublishersSample().First();
+            model.CheckPassedPublisherAsync(publisher);
+            model.SetErrorsChangedEventAsync();
+
+            model.InputModel.Name = "";
+
+            Assert.False(model.CanSave);
+        }
+
+        [Fact]
+        public void CanExecuteChanged_CreationDateTimeIsMin_CanSaveIsFalse()
+        {
+            var model = new AddEditPublisherViewModel(null, null);
+            var publisher = TestData.GetPublishersSample().First();
+            model.CheckPassedPublisherAsync(publisher);
+            model.SetErrorsChangedEventAsync();
+
+            model.Publisher.CreationDateTime = DateTime.MinValue;
+
+            Assert.False(model.CanSave);
+        }
+        
     }
 }
