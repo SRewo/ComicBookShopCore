8fd62cc534dc39bfbe5de28d8565351ea8d1a4e2
SRewo <maoam97@gmail.com>
2019-08-20 16:01:48 +0200

    Add async repository

---
+++ ComicBookShopCore.Data/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComicBookShopCore.Data.Interfaces
+{
+    public interface IAsyncRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ ComicBookShopCore.Data/Repositories/SqlAsyncRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ComicBookShopCore.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComicBookShopCore.Data.Repositories
+{
+    public class SqlAsyncRepository<T> : IAsyncRepository<T> where T : class
+    {
+        private DbContext _context;
+        private DbSet<T> _dbSet;
+
+        public SqlAsyncRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync().ConfigureAwait(true);
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id).ConfigureAwait(true);
+        }
+
+        public Task AddAsync(T entity)
+        {
+            _dbSet.Add(entity);
+            return _context.SaveChangesAsync();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            return _context.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            return _context.SaveChangesAsync();
+        }
+    }
+}
