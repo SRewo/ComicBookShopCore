a20284510c0390dc2b6fd57ff9f1251d4875835b
SRewo <maoam97@gmail.com>
2019-10-03 16:58:07 +0200

    Add user service tests + login fix

---
+++ ComicBookShopCore.Services.Tests/User/UserServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extras.Moq;
+using AutoMapper;
+using ComicBookShopCore.Services.User;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using Xunit;
+
+namespace ComicBookShopCore.Services.Tests.User
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public async Task Login_ValidCall()
+        {
+            var userStore = new Mock<IUserStore<Data.User>>();
+            var managerMock = new Mock<UserManager<Data.User>>(userStore.Object, null, null, null, null, null, null, null, null);
+            var user = new Data.User() {UserName = "user", Id = new Guid().ToString()};
+            managerMock.Setup(x => x.FindByNameAsync("user"))
+                .ReturnsAsync(user);
+            managerMock.Setup(x => x.GetRolesAsync(user)).ReturnsAsync(new [] {"Admin"});
+            managerMock.Setup(x => x.CheckPasswordAsync(user, "123")).ReturnsAsync(true);
+            var mapperConfig = new MapperConfiguration(mc => mc.AddProfile(new MapperProfile()));
+            var service = new UserService(managerMock.Object, mapperConfig.CreateMapper());
+
+            var result = await service.Login("user", "123");
+
+            Assert.IsType<UserTokenDto>(result);
+            Assert.Equal(user.UserName, result.Login);
+            Assert.Equal(user.Id, result.Id);
+            Assert.Equal("Admin", result.Role);
+        }
+
+        [Fact]
+        public async Task Login_InvalidUsername_ReturnsNull()
+        {
+            var userStore = new Mock<IUserStore<Data.User>>();
+            var managerMock =
+                new Mock<UserManager<Data.User>>(userStore.Object, null, null, null, null, null, null, null, null);
+            var mapperConfig = new MapperConfiguration(mc => mc.AddProfile(new MapperProfile()));
+            var service = new UserService(managerMock.Object, mapperConfig.CreateMapper());
+
+            var result = await service.Login("user", "123");
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task Login_InvalidPassword_ReturnsNull()
+        {
+            var userStore = new Mock<IUserStore<Data.User>>();
+            var managerMock =
+                new Mock<UserManager<Data.User>>(userStore.Object, null, null, null, null, null, null, null, null);
+            var user = new Data.User();
+            managerMock.Setup(x => x.FindByNameAsync("user")).ReturnsAsync(user);
+            managerMock.Setup(x => x.CheckPasswordAsync(user, "123")).ReturnsAsync(false);
+            var mapper = new Mock<IMapper>();
+            var service = new UserService(managerMock.Object, mapper.Object);
+
+            var result = await service.Login("user", "123");
+
+            Assert.Null(result);
+        }
+    }
+}
--- ComicBookShopCore.Services/User/UserService.cs
+++ ComicBookShopCore.Services/User/UserService.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Identity;
@@ -6,15 +8,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ComicBookShopCore.Services.User
 {
-    public interface IUserService
-    {
-        Task<UserTokenDto> Login(string username, string password);
-    }
-
     public class UserService : IUserService
     {
-        private UserManager<Data.User> _manager;
-        private IMapper _mapper;
+        private readonly UserManager<Data.User> _manager;
+        private readonly IMapper _mapper;
 
         public UserService(UserManager<Data.User> manager, IMapper mapper)
         {
@@ -24,19 +21,36 @@ namespace ComicBookShopCore.Services.User
 
         public async Task<UserTokenDto> Login(string username, string password)
         {
-            var user = await _manager.Users.FirstOrDefaultAsync(x => x.UserName == username);
+            var user = await _manager.FindByNameAsync(username);
 
             if (user == null) return null;
 
             var result = await _manager.CheckPasswordAsync(user, password);
             if (!result)
-                user = null;
+                 return null;
 
 
             var dto = _mapper.Map<UserTokenDto>(user);
             var roles = await _manager.GetRolesAsync(user);
             dto.Role = roles.First();
             return dto;
+        }
+
+        public async Task Register(UserRegisterDto userDto)
+        {
+            if (userDto.Password != userDto.ConfirmPassword)
+                throw new ValidationException("The password and confirmation password do not match.");
+
+            var user = _mapper.Map<Data.User>(userDto);
+
+            var result = await _manager.CreateAsync(user, userDto.Password);
+
+            if (!result.Succeeded)
+            {
+                var errors = new StringBuilder();
+                foreach (var identityError in result.Errors) errors.AppendLine(identityError.Description);
+                throw new ValidationException(errors.ToString());
+            }
         }
 
     }
